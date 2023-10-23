f2c8a224828b4b3b3c72ce30a04cb3bcb5034d37
kooopppp <33962369+kooopppp@users.noreply.github.com>
2023-10-23 15:48:36 +0200

    Add files via upload

---
+++ TP1/tp1ex1.py
@@ -0,0 +1,8 @@
+nom ='toto'
+prenom ='titi'
+math = 15
+anglais = 12
+info = 10.5
+prmotion = 2023
+var = (math + anglais + info) / 3
+f" L’étudiant {nom} {prenom} de la promotion {promotion} a une moyenne de {var}"
---
+++ TP1/tp1ex3.py
@@ -0,0 +1,10 @@
+
+print("minutes :" )
+m=int(input())
+print("heures :")
+h=int(input())
+print("jour :")
+j=int(input())
+var = (j*24*60) + (h*60) + m
+print(var)
+
