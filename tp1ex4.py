e40a270641e7da44c964df27e045879ec49682c1
kooopppp <33962369+kooopppp@users.noreply.github.com>
2023-10-23 15:46:46 +0200

    Delete tp1ex4.py

--- tp1ex4.py
+++
@@ -1,8 +0,0 @@
-minutes= int(input("tapez le nombre de minutes : "))
-minute = minutes//60//24//31
-print(minute, "minutes restantes")
-jour= minutes//60//24
-print(jour, 'jours')
-
-mois= jour//31
-print(mois, 'mois')
