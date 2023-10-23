86382e4229b93d68b932106f70b3a9a0761b6ce8
kooopppp <33962369+kooopppp@users.noreply.github.com>
2023-10-23 15:47:26 +0200

    Delete tp1ex1.py

--- tp1ex1.py
+++
@@ -1,8 +0,0 @@
-nom ='toto'
-prenom ='titi'
-math = 15
-anglais = 12
-info = 10.5
-prmotion = 2023
-var = (math + anglais + info) / 3
-f" L’étudiant {nom} {prenom} de la promotion {promotion} a une moyenne de {var}"
